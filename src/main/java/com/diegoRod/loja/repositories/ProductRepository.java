7d87df37450a3e50bcf837e42cfdbfb6837dfc02
Diego Almeida <diegorodalmeida@gmail.com>
2022-09-05 12:33:30 -0300

    Product

---
+++ src/main/java/com/diegoRod/loja/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.diegoRod.loja.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegoRod.loja.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/diegoRod/loja/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.diegoRod.loja.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegoRod.loja.entities.Product;
+import com.diegoRod.loja.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product>findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
